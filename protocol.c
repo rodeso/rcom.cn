c00b195476f0daff7e2bad023aca55ffdd82b1bb
Rodrigo de Sousa <rodrigodesousa@proton.me>
2024-12-13 18:46:50 +0000

    added printf

--- protocol.c
+++ protocol.c
@@ -275,6 +275,7 @@ int request_file(int sockfd, char *resource) {
     sprintf(buffer, "RETR %s\r\n", resource);
     send(sockfd, buffer, strlen(buffer), 0);
     recv(sockfd, buffer, sizeof(buffer), 0);
+    printf("Server response: %s\n", buffer);
     return strstr(buffer, "150") ? ERR_SUCCESS : ERR_RETR_FAIL;
 }
 
